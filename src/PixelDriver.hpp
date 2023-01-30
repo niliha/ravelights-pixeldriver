0a66c792ae152e635952c2976ba169bbefb7fc1f
niliha <75397148+niliha@users.noreply.github.com>
2023-01-30 23:50:39 +0100

    Clean up code

--- src/PixelDriver.hpp
+++ src/PixelDriver.hpp
@@ -9,20 +9,12 @@
 #include <numeric>
 #include <set>
 
-template <int PIN_COUNT, const std::array<int, PIN_COUNT> &PINS, EOrder RGB_ORDER = RGB>
-class PixelDriver {
+template <int PIN_COUNT, const std::array<int, PIN_COUNT> &PINS, EOrder RGB_ORDER = RGB> class PixelDriver {
  public:
-    PixelDriver(const std::array<int, PIN_COUNT> &lightsPerPin, int pixelsPerLight = 144,
-                uint8_t maxBrightness = 255, bool debug = false)
-        : MAX_BRIGHTNESS_(maxBrightness), DEBUG_(debug), PIXELS_PER_LIGHT_(pixelsPerLight),
-          showSemaphore_(xSemaphoreCreateBinary()), showFinishedSem_(xSemaphoreCreateBinary()),
-          frameQueue_(3), framePtr_(std::make_shared<std::vector<CRGB>>()) {
-        // The watchdog on core 0 is not reset anymore, since the idle task is not resumed.
-        // It is disabled to avoid watchdog timeouts (resulting in a reboot).
-        disableCore0WDT();
-
-        // Improves UDP throughput drastically
-        WiFi.setSleep(false);
+    PixelDriver(const std::array<int, PIN_COUNT> &lightsPerPin, float framesPerSecond = 20, int frameQueueCapacity = 3,
+                int pixelsPerLight = 144, bool debug = false)
+        : DEBUG_(debug), PIXELS_PER_LIGHT_(pixelsPerLight), FRAME_PERIOD_MS_(1000 / framesPerSecond),
+          artnetFrame_(std::make_shared<std::vector<CRGB>>()), frameQueue_(frameQueueCapacity) {
 
         configure(lightsPerPin);
     }
@@ -45,121 +37,110 @@ class PixelDriver {
     }
 
     void start() {
-        assert(showSemaphore_ != nullptr && "showSemaphore_ must not be null!");
         // Start artnet task on core 0 (together with the WIFI service)
-        xTaskCreatePinnedToCore(artnetTaskWrapper, "artnetTask", 4096, this, 1, NULL, 0);
+        xTaskCreatePinnedToCore(staticArtnetTask, "artnetTask", 4096, this, 1, NULL, 0);
+
         // Start the fastled task on core 1.
         // Therefore it is not affected by WIFI interrupts from core 0 while writing to the LEDs
         // (avoiding flickering).
-        xTaskCreatePinnedToCore(fastledTaskWrapper, "fastledTask", 4096, this, 1, NULL, 1);
+        xTaskCreatePinnedToCore(staticFastledTask, "fastledTask", 4096, this, 1, NULL, 1);
     }
 
  private:
-    /* "Constants" */
-    // The number of individually addressable pixels per "light"
-    const uint8_t MAX_BRIGHTNESS_;
+    // Whether to print debug information
     const bool DEBUG_;
-    const int PIXELS_PER_LIGHT_;
-    // Number of required Artnet universes. Set in configure().
-    int UNIVERSE_COUNT_;
-    // Total number of pixels across all lights. Set in configure().
-    int PIXEL_COUNT_;
 
-    /* Data structures for FastLed and ArtnetWifi */
+    // The number of individually addressable pixels per ravelight
+    const int PIXELS_PER_LIGHT_;
+
+    // The period in milliseconds to wait between writing subsequent frames to the LEDs.
+    const int FRAME_PERIOD_MS_;
+
+    // Total number of pixels across all lights. Set in configure().
+    int pixelCount_;
+
     // The vector holding color values for each pixel.
     // It's size is set in configure() and must not be changed afterwards!
-    std::vector<CRGB> pixels_;
-    std::set<int> receivedUniverses_;
-    int lastUniverse_ = -1;
+    std::vector<CRGB> fastLedPixels_;
 
     // The ArtnetWifi instance
     ArtnetWifi artnet_;
 
-    // Binary semaphore that is initialized (already taken) in initializer list.
-    // It is used by the artnet task to signal the fastled task to write to the leds once all
-    // universes of a single frame were received.
-    SemaphoreHandle_t showSemaphore_;
+    // Number of required Artnet universes. Set in configure().
+    int universeCount_;
 
-    // Binary semaphore that is initialized (already taken) in initializer list.
-    // It is used by the fastled task to signal the artnet task that is has finished writing to the
-    // LEDs.
-    SemaphoreHandle_t showFinishedSem_;
+    // Set of universe indices indicating which universes of the current frame have been received yet
+    std::set<int> receivedUniverses_;
 
+    // Queue for storing incoming Artnet frames
+    FrameQueue<std::shared_ptr<std::vector<CRGB>>> frameQueue_;
+
+    // Mutex for synchronizing access to the frame queue by the ArtNet and FastLED task
     std::mutex queue_mutex_;
 
-    FrameQueue<std::shared_ptr<std::vector<CRGB>>> frameQueue_;
-    std::shared_ptr<std::vector<CRGB>> framePtr_;
+    // Pointer referencing the frame currently filled by the ArtNet task
+    std::shared_ptr<std::vector<CRGB>> artnetFrame_;
 
     // Used for debugging the time between frames.
     unsigned long timeOfLastFrame_ = 0;
 
     void configure(const std::array<int, PIN_COUNT> &lightsPerPin) {
-        PIXEL_COUNT_ =
-            PIXELS_PER_LIGHT_ * std::accumulate(lightsPerPin.begin(), lightsPerPin.end(), 0);
-        pixels_.resize(PIXEL_COUNT_);
-        UNIVERSE_COUNT_ = std::ceil((PIXEL_COUNT_ * 3) / static_cast<float>(512));
+        // The watchdog on core 0 is not reset anymore, since the idle task is not resumed.
+        // It is disabled to avoid watchdog timeouts (resulting in a reboot).
+        disableCore0WDT();
+
+        // Improves UDP throughput drastically
+        WiFi.setSleep(false);
+
+        pixelCount_ = PIXELS_PER_LIGHT_ * std::accumulate(lightsPerPin.begin(), lightsPerPin.end(), 0);
+        fastLedPixels_.resize(pixelCount_);
+        artnetFrame_->resize(pixelCount_);
+        universeCount_ = std::ceil((pixelCount_ * 3) / static_cast<float>(512));
+
         setupFastled(lightsPerPin);
         setArtnetCallback();
     }
 
     void setupFastled(const std::array<int, PIN_COUNT> &lightsPerPin) {
-        static_assert(PIN_COUNT == 4 && "setupFastLed() is hardcoded to handle exactly 4 pins!",
-                      "");
+        static_assert(PIN_COUNT == 4, "setupFastLed() is hardcoded to handle exactly 4 pins!");
         // We can't use a loop here since addLeds() template parameters must be known at
         // compile-time
         int pixelOffset = 0;
         if (lightsPerPin[0] > 0) {
-            FastLED.addLeds<WS2812, PINS[0], RGB_ORDER>(pixels_.data(), pixelOffset,
+            FastLED.addLeds<WS2812, PINS[0], RGB_ORDER>(fastLedPixels_.data(), pixelOffset,
                                                         lightsPerPin[0] * PIXELS_PER_LIGHT_);
             pixelOffset += lightsPerPin[0] * PIXELS_PER_LIGHT_;
         }
         if (lightsPerPin[1] > 0) {
-            FastLED.addLeds<WS2812, PINS[1], RGB_ORDER>(pixels_.data(), pixelOffset,
+            FastLED.addLeds<WS2812, PINS[1], RGB_ORDER>(fastLedPixels_.data(), pixelOffset,
                                                         lightsPerPin[1] * PIXELS_PER_LIGHT_);
             pixelOffset += lightsPerPin[1] * PIXELS_PER_LIGHT_;
         }
         if (lightsPerPin[2] > 0) {
-            FastLED.addLeds<WS2812, PINS[2], RGB_ORDER>(pixels_.data(), pixelOffset,
+            FastLED.addLeds<WS2812, PINS[2], RGB_ORDER>(fastLedPixels_.data(), pixelOffset,
                                                         lightsPerPin[2] * PIXELS_PER_LIGHT_);
             pixelOffset += lightsPerPin[2] * PIXELS_PER_LIGHT_;
         }
         if (lightsPerPin[3] > 0) {
-            FastLED.addLeds<WS2812, PINS[3], RGB_ORDER>(pixels_.data(), pixelOffset,
+            FastLED.addLeds<WS2812, PINS[3], RGB_ORDER>(fastLedPixels_.data(), pixelOffset,
                                                         lightsPerPin[3] * PIXELS_PER_LIGHT_);
             pixelOffset += lightsPerPin[3] * PIXELS_PER_LIGHT_;
         }
-        // Set maximum brightness (0 - 255)
-        FastLED.setBrightness(MAX_BRIGHTNESS_);
-        // FastLED.setMaxRefreshRate(50);
     }
 
     void setArtnetCallback() {
-        artnet_.setArtDmxFunc(
-            [this](uint16_t universeIndex, uint16_t length, uint8_t sequence, uint8_t *data) {
-                this->onDmxFrame(universeIndex, length, sequence, data);
-            });
-    }
-
-    void artnetTask() {
-        Serial.print("artnetTask: started on core ");
-        Serial.println(xPortGetCoreID());
-        artnet_.begin();
-        while (true) {
-            uint16_t result = artnet_.read();
-        }
+        artnet_.setArtDmxCallback(nullptr);
+        artnet_.setArtDmxFunc([this](uint16_t universeIndex, uint16_t length, uint8_t sequence, uint8_t *data) {
+            this->onDmxFrame(universeIndex, length, sequence, data);
+        });
     }
 
     void fastledTask() {
         Serial.print("fastledTask: started on core ");
         Serial.println(xPortGetCoreID());
-        const int FRAME_TIME_MS = 1000 / 20;
 
-        auto lastMillis = millis();
         while (true) {
-            if (millis() - lastMillis < FRAME_TIME_MS) {
-                continue;
-            }
-            lastMillis = millis();
+            auto millisBefore = millis();
 
             // Update pixel buffer if there is a new frame in the queue
             {
@@ -167,39 +148,46 @@ class PixelDriver {
                 std::lock_guard<std::mutex> lock(queue_mutex_);
 
                 if (!frameQueue_.isEmpty()) {
-                    Serial.println("frameQueue not empty!");
-                    // Move oldest frame from the queue into pixel buffer
-                    pixels_ = *frameQueue_.front();
+                    // Copy oldest frame from the queue into pixel buffer
+                    fastLedPixels_ = *frameQueue_.front();
+                    // Pop pointer to oldest frame from the queue
                     frameQueue_.pop();
-                } else {
-                    Serial.println("frameQueue is empty!");
                 }
             }
 
             // Write pixel buffer to the LEDs
             FastLED.show();
+
+            // Wait until a frame period has passed
+            auto passedMillis = millis() - millisBefore;
+            auto millisToWait = FRAME_PERIOD_MS_ - passedMillis;
+            if (millisToWait < 0) {
+                Serial.print("WARN: FPS to high! Would need to wait negative time: ");
+                Serial.println(millisToWait);
+                return;
+            }
+            delay(passedMillis);
         }
     }
 
-    // Static wrappers around the task function such that we can use non-static member functions
-    static void fastledTaskWrapper(void *thisPointer) {
-        static_cast<PixelDriver<PIN_COUNT, PINS, RGB_ORDER> *>(thisPointer)->fastledTask();
+    static void staticFastledTask(void *thisPointer) {
+        static_cast<PixelDriver *>(thisPointer)->fastledTask();
     }
 
-    static void artnetTaskWrapper(void *thisPointer) {
-        // static_cast<PixelDriver<PIN_COUNT, PINS, RGB_ORDER> *>(_this)->artnetTask();
+    void artnetTask() {
+        Serial.print("artnetTask: started on core ");
+        Serial.println(xPortGetCoreID());
+
+        artnet_.begin();
+
+        while (true) {
+            // This calls onDmxFrame() whenever a ArtDMX packet is received
+            auto result = artnet_.read();
+        }
+    }
+
+    static void staticArtnetTask(void *thisPointer) {
         static_cast<PixelDriver *>(thisPointer)->artnetTask();
-    }
-
-    void setChannel(uint16_t universeIndex, int channelIndex, uint8_t value) {
-        // integer division will round off to nearest neighbor;
-        unsigned pixelIndex = (universeIndex * 512 + channelIndex) / 3;
-        // 0 -> first channel, 1 -> second channel, 2 -> third channel
-        unsigned rgbChannelIndex = (universeIndex * 512 + channelIndex) % 3;
-        if (pixelIndex >= PIXEL_COUNT_) {
-            return;
-        }
-        (*framePtr_)[pixelIndex][rgbChannelIndex] = value;
     }
 
     void onDmxFrame(uint16_t universeIndex, uint16_t length, uint8_t sequence, uint8_t *data) {
@@ -209,42 +197,53 @@ class PixelDriver {
             Serial.print(" With size ");
             Serial.println(length);
         }
-        if (universeIndex >= UNIVERSE_COUNT_) {
+
+        if (universeIndex >= universeCount_) {
             if (DEBUG_) {
-                Serial.println("Error: Received invalid universe index!");
+                Serial.print("Error: Received invalid universe index: ");
+                Serial.println(universeIndex);
             }
             return;
-        }
-        if (universeIndex != (lastUniverse_ + 1) % UNIVERSE_COUNT_) {
-            return;
-        }
-        if (universeIndex == 0) {
-            // This will reassign the shared ptr.
-            // If the old framePtr_ has a use_count of 1, its underlying vector will be
-            // destroyed
-            framePtr_ = std::make_shared<std::vector<CRGB>>(PIXEL_COUNT_);
         }
 
         // Store which universe has got in
         receivedUniverses_.insert(universeIndex);
-        lastUniverse_ = universeIndex;
+
         // Read universe and put into the right part of the display buffer
         for (unsigned channelIndex = 0; channelIndex < length; channelIndex++) {
             setChannel(universeIndex, channelIndex, data[channelIndex]);
         }
-        // Write to leds if all universes were received
-        if (receivedUniverses_.size() == UNIVERSE_COUNT_) {
+
+        // All data for this frame has been received
+        if (receivedUniverses_.size() == universeCount_) {
             if (DEBUG_) {
                 Serial.print("Time since last frame: ");
                 Serial.println(millis() - timeOfLastFrame_);
             }
             timeOfLastFrame_ = millis();
-            //  Reset receivedUniverses
+
+            // Reset information about received universes
             receivedUniverses_.clear();
+
+            // Put the filled frame into the queue such that the FasLED task can consume it
             {
                 std::lock_guard<std::mutex> lock(queue_mutex_);
-                frameQueue_.push(framePtr_);
+                frameQueue_.push(artnetFrame_);
+
+                // Create a new vector for the next frame.
+                artnetFrame_ = std::make_shared<std::vector<CRGB>>(pixelCount_);
             }
         }
+    }
+
+    void setChannel(uint16_t universeIndex, int channelIndex, uint8_t value) {
+        // integer division will round off to nearest neighbor;
+        unsigned pixelIndex = (universeIndex * 512 + channelIndex) / 3;
+        // 0 -> first channel, 1 -> second channel, 2 -> third channel
+        unsigned rgbChannelIndex = (universeIndex * 512 + channelIndex) % 3;
+        if (pixelIndex >= pixelCount_) {
+            return;
+        }
+        (*artnetFrame_)[pixelIndex][rgbChannelIndex] = value;
     }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -29,12 +29,17 @@ void setup() {
     }
     */
 
-    PixelDriver<MAX_PIN_COUNT, PINS, RGB_ORDER> pixelDriver(lightsPerPin, 144, 255,false);
+    // PixelDriver<MAX_PIN_COUNT, PINS, RGB_ORDER> pixelDriver(lightsPerPin, 144, 255, false);
+    PixelDriver<MAX_PIN_COUNT, PINS, RGB_ORDER> pixelDriver(lightsPerPin, 20, 3, 144, true);
+
     pixelDriver.testLeds();
     Network::initWifiAccessPoint(WifiCredentials::ssid, WifiCredentials::password);
+
     pixelDriver.start();
+
     while (true) {
         // Stay in setup() such that stack frame is not popped
+        delay(1000);
     }
 }
 
