e165196a443b05e29840335d9b66bb87b045d5fb
niliha <75397148+niliha@users.noreply.github.com>
2023-11-29 23:01:33 +0100

    Remove obsolete wifi credentials

--- src/WifiCredentials.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <string>
-
-namespace WifiCredentials {
-static const std::string ssid = "the wifi's ssid";
-static const std::string password = "the wifi's password";
-}  // namespace WifiCredentials
