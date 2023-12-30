3e76ffb2522ac48893264442ad28d97ce16efaed
niliha <75397148+niliha@users.noreply.github.com>
2023-12-30 18:08:11 +0100

    Fix OutputConfig initialization

--- src/config/OutputConfig.cpp
+++ src/config/OutputConfig.cpp
@@ -2,16 +2,6 @@
 
 #include <numeric>
 
-
-OutputConfig::OutputConfig(std::initializer_list<uint32_t> values) : std::array<uint32_t, OUTPUT_COUNT>() {
-    assert(values.size() == OUTPUT_COUNT);
-}
-
 int OutputConfig::getPixelCount() const {
     return std::accumulate(this->begin(), this->end(), 0);
-}
-
-bool OutputConfig::operator==(const OutputConfig &other) const {
-    return static_cast<const std::array<uint32_t, OUTPUT_COUNT> &>(*this) ==
-           static_cast<const std::array<uint32_t, OUTPUT_COUNT> &>(other);
 }
