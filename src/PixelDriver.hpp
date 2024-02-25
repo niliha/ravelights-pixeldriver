9a8c87005c69af1730a77ad9691709c33557ab2c
niliha <75397148+niliha@users.noreply.github.com>
2024-02-25 12:14:11 +0100

    [WIP] Fix interrupt latency

--- src/PixelDriver.hpp
+++ src/PixelDriver.hpp
@@ -12,17 +12,19 @@
 class PixelDriver {
  public:
     PixelDriver(std::vector<std::shared_ptr<AbstractInterfaceHandler>> &interfaces,
-                BlockingRingBuffer<PixelFrame> &artnetQueue, AbstractPixelHandler &pixelHandler);
+                BlockingRingBuffer<PixelFrame> &artnetQueue, AbstractPixelHandler &pixelHandler,
+                int interfaceTaskCore = 0, int interfaceTaskPriority = tskIDLE_PRIORITY, int pixelTaskCore = 1,
+                int pixelTaskPriority = 19);
 
     void start();
 
-    static const int INTERFACE_TASK_PRIORITY_ = tskIDLE_PRIORITY;
-    static const int INTERFACE_TASK_CORE_ = 0;
-
-    static const int PIXEL_TASK_PRIORITY_ = 19;
-    static const int PIXEL_TASK_CORE_ = 1;
-
  private:
+    const int INTERFACE_TASK_CORE_;
+    const int INTERFACE_TASK_PRIORITY_;
+
+    const int PIXEL_TASK_CORE_;
+    const int PIXEL_TASK_PRIORITY_;
+
     AbstractPixelHandler &pixelHandler_;
     std::vector<std::shared_ptr<AbstractInterfaceHandler>> &interfaces_;
     BlockingRingBuffer<PixelFrame> &artnetQueue_;
